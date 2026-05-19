682db89ff24fedbfc14a7f1477b026c2c709c9f2
b0kar <n.rehcuob@gmail.com>
2026-05-19 16:01:57 +0200

    Update register.go

--- register.go
+++ register.go
@@ -2,21 +2,21 @@ package xk6_entry
 
 import (
 	"github.com/zagaris/xk6-counter/counter"
-	"go.k6.io/k6/js/modules"
+	"go.k6.io/k6/v2/js/modules"
 )
 
 type CounterModule struct{}
 
-func (m *CounterModule) Increment() int {
-	return counter.Increment()
+func (m *CounterModule) up() int {
+	return counter.up()
 }
 
-func (m *CounterModule) Get() int {
-	return counter.Get()
+func (m *CounterModule) get() int {
+	return counter.get()
 }
 
-func (m *CounterModule) Reset() {
-	counter.Reset()
+func (m *CounterModule) reset() {
+	counter.reset()
 }
 
 func init() {
